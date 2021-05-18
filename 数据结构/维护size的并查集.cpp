4e9688252d8e35ee83eaf23134a213de4b4c1d30
Dezhonger <373440955@qq.com>
2021-05-18 23:00:14 +0800

    Create 维护size的并查集.cpp

---
+++ 数据结构/维护size的并查集.cpp
@@ -0,0 +1,20 @@
+int p[N], size[N];
+//p[]存储每个点的祖宗节点, size[]只有祖宗节点的有意义，表示祖宗节点所在集合中的点的数量
+
+// 返回x的祖宗节点
+int find(int x)
+{
+    if (p[x] != x) p[x] = find(p[x]);
+    return p[x];
+}
+
+// 初始化，假定节点编号是1~n
+for (int i = 1; i <= n; i ++ )
+{
+    p[i] = i;
+    size[i] = 1;
+}
+
+// 合并a和b所在的两个集合：
+size[find(b)] += size[find(a)];
+p[find(a)] = find(b);
