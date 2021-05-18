11264bbff32260eb306aac7fcda2bd39eaa67384
Dezhonger <373440955@qq.com>
2021-05-18 22:59:35 +0800

    Create 并查集.cpp

---
+++ 数据结构/并查集.cpp
@@ -0,0 +1,14 @@
+int p[N]; //存储每个点的祖宗节点
+
+// 返回x的祖宗节点
+int find(int x)
+{
+    if (p[x] != x) p[x] = find(p[x]);
+    return p[x];
+}
+
+// 初始化，假定节点编号是1~n
+for (int i = 1; i <= n; i ++ ) p[i] = i;
+
+// 合并a和b所在的两个集合：
+p[find(a)] = find(b);
