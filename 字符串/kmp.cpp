a76d11774fc53e7b5cd7c38e549a72b70d4772e0
Dezhonger <373440955@qq.com>
2021-05-26 10:56:37 +0800

    add kmp.cpp

---
+++ 字符串/kmp.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define reps(i, a, b) for(int i = (a); i <= (b); i++)
+#define F first
+#define S second
+
+using namespace std;
+
+const int N = 100010, M = 1000010;
+
+int n, m;
+int ne[N];
+char s[M], p[N];
+
+//样例输入:
+//3
+//aba
+//5
+//ababa
+//样例输出:
+//0 2 
+int main()
+{
+    //下标从1开始计算
+    cin >> n >> p + 1 >> m >> s + 1;
+
+    for (int i = 2, j = 0; i <= n; i ++ )
+    {
+        while (j && p[i] != p[j + 1]) j = ne[j];
+        if (p[i] == p[j + 1]) j ++ ;
+        ne[i] = j;
+    }
+    //for (int i = 1; i <= n; i++) cout << ne[i] << endl;
+    for (int i = 1, j = 0; i <= m; i ++ )
+    {
+        while (j && s[i] != p[j + 1]) j = ne[j];
+        if (s[i] == p[j + 1]) j ++ ;
+        if (j == n)
+        {
+            printf("%d ", i - n);
+            j = ne[j];
+        }
+    }
+
+    return 0;
+}
