c5b4dfd394856498efb61786ed074d60afe09ca4
Dezhonger <373440955@qq.com>
2021-07-04 22:55:29 +0800

    Create 字符串hash.cpp

---
+++ 字符串/字符串hash.cpp
@@ -0,0 +1,47 @@
+//https://www.acwing.com/problem/content/843/
+//大致题意：给定一个长度为 n 的字符串，再给定 m 个询问，每个询问包含四个整数 l1,r1,l2,r2，请你判断 [l1,r1] 和 [l2,r2] 这两个区间所包含的字符串子串是否完全相同。
+
+//注意：代码实现中，下标都是从1开始的
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef unsigned long long ULL;
+
+const int N = 100010, P = 131;
+
+int n, m;
+char str[N];
+ULL h[N], p[N];
+
+ULL get(int l, int r)
+{
+    return h[r] - h[l - 1] * p[r - l + 1];
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    scanf("%s", str + 1);
+
+    p[0] = 1;
+    for (int i = 1; i <= n; i ++ )
+    {
+        h[i] = h[i - 1] * P + str[i];
+        p[i] = p[i - 1] * P;
+    }
+
+    while (m -- )
+    {
+        int l1, r1, l2, r2;
+        scanf("%d%d%d%d", &l1, &r1, &l2, &r2);
+
+        if (get(l1, r1) == get(l2, r2)) puts("Yes");
+        else puts("No");
+    }
+
+    return 0;
+}
+
