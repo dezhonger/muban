4ff76d10cd7e51b2fda5346a1aafdd0833cb79f1
Dezhonger <373440955@qq.com>
2021-07-17 00:32:52 +0800

    Create 求组合数.cpp

---
+++ 数学/求组合数.cpp
@@ -0,0 +1,22 @@
+typedef long long LL;
+int mod = (int)1e9 + 7;
+LL powmod(LL a,LL b) {
+	LL res = 1;
+	a %= mod;
+	assert(b >= 0);
+	for (; b; b >>= 1) {
+		if (b & 1)res = res * a % mod;
+		a = a * a % mod;
+	}
+	return res;
+}
+
+int inv(int n){
+    return powmod(n, mod - 2);
+}
+int C(int m, int n){
+    int res = 1;
+    for(int i = 0; i < n; i += 1) res = 1LL * res * (m - i) % mod;
+    for(int i = 1; i <= n; i += 1) res = 1LL * res * inv(i) % mod;
+    return res;
+}
