5859c548515b57968af94399663f3803ce32548e
Dezhonger <373440955@qq.com>
2021-07-17 00:34:02 +0800

    Create 线性筛素数.cpp

---
+++ 数学/线性筛素数.cpp
@@ -0,0 +1,15 @@
+int primes[N], cnt;
+bool st[N];
+
+void get_primes(int n)
+{
+    for (int i = 2; i <= n; i ++ )
+    {
+        if (!st[i]) primes[cnt ++ ] = i;
+        for (int j = 0; primes[j] <= n / i; j ++ )
+        {
+            st[primes[j] * i] = true;
+            if (i % primes[j] == 0) break;
+        }
+    }
+}
