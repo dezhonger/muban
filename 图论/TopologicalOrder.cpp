60681652354798af4dba9b58b2cdec5469d35f1e
Dezhonger <373440955@qq.com>
2023-06-29 23:32:50 +0800

    Update TopologicalOrder.cpp

--- 图论/TopologicalOrder.cpp
+++ 图论/TopologicalOrder.cpp
@@ -22,3 +22,37 @@ bool topsort()
     // 如果所有点都入队了，说明存在拓扑序列；否则不存在拓扑序列。
     return tt == n - 1;
 }
+
+
+
+// 另一种写法，更简单看起来
+
+vector<vector<int>> e(n);   // 邻接表
+vector<int> indeg(n);   // 入度表
+// 预处理
+for (const auto& edge: edges) {
+    e[edge[0]].push_back(edge[1]);
+    ++indeg[edge[1]];
+}
+// 广度优先搜索求解拓扑排序
+queue<int> q;
+for (int i = 0; i < n; ++i) {
+    if (!indeg[i]) {
+        q.push(i);
+    }
+}
+while (!q.empty()) {
+    int u = q.front();
+    q.pop();
+    for (int v: e[u]) {
+        // 更新子节点的祖先哈希表
+        anc[v].insert(u);
+        for (int i: anc[u]) {
+            anc[v].insert(i);
+        }
+        --indeg[v];
+        if (!indeg[v]) {
+            q.push(v);
+        }
+    }
+}
