f31ceffa7dcf6398bd8d8ad95fc3ea7a8b6bb153
Dezhonger <373440955@qq.com>
2025-03-24 22:48:54 +0800

    Create template_cpp.cpp

---
+++ misc/template_cpp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef LOCAL
+#include "C:\zwl\files\docs\debug.h"
+#else
+#define debug(...) 42
+#endif
+typedef long long LL;
+typedef unsigned long long ULL;
+ 
+void solve() {
+
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
