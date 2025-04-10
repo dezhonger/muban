4134801d974954f136f78ff8bde3f6d171fab68a
Dezhonger <373440955@qq.com>
2025-04-10 14:17:32 +0800

    Update 树状数组.cpp

--- 数据结构/树状数组.cpp
+++ 数据结构/树状数组.cpp
@@ -22,3 +22,30 @@ struct Bit {
         return sum(right) - sum(left - 1);
     }
 };
+
+//模板化
+template <typename T>
+struct Bit {
+    int n;
+    std::vector<T> tr;
+
+    Bit(int n) : n(n) { tr = std::vector<T>(n + 1, 0); }
+
+    int lowbit(int x) { return x & (-x); }
+
+    void add(int pos, T x) {
+        for (int i = pos; i <= n; i += lowbit(i))
+            tr[i] += x;
+    }
+
+    T sum(int pos) {
+        T res = 0;
+        for (int i = pos; i; i -= lowbit(i))
+            res += tr[i];
+        return res;
+    }
+
+    T query(int left, int right) {
+        return sum(right) - sum(left - 1);
+    }
+};
