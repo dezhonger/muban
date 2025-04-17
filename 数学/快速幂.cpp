68a765ecbc2477593ee1bbb1fc52a8cbdd7fd9f0
dezhonger <csdisassembly@gmail.com>
2025-04-17 23:31:01 +0800

    space

--- 数学/快速幂.cpp
+++ 数学/快速幂.cpp
@@ -1,12 +1,13 @@
 typedef long long LL;
 int mod = (int)1e9 + 7;
-LL powmod(LL a,LL b) {
-	LL res = 1;
-	a %= mod;
-	assert(b >= 0);
-	for (; b; b >>= 1) {
-		if (b & 1)res = res * a % mod;
-		a = a * a % mod;
-	}
-	return res;
+LL powmod(LL a, LL b) {
+    LL res = 1;
+    a %= mod;
+    assert(b >= 0);
+    for (; b; b >>= 1) {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+    }
+    return res;
 }
