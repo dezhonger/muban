ee9f563edfd64c220209eb8ae195b0a2440304e7
Dezhonger <373440955@qq.com>
2025-04-27 03:23:39 +0800

    Update kmp.cpp

--- 字符串/kmp.cpp
+++ 字符串/kmp.cpp
@@ -46,3 +46,33 @@ int main()
 
     return 0;
 }
+
+
+
+
+// muban
+const int N = 100010, M = 1000010;
+int ne[N];
+vector<int> solve(int n, string p, int m, string s) {
+    vector<int> ans;
+    memset(ne, 0, sizeof ne);
+    for (int i = 2, j = 0; i <= n; i++) {
+        while (j && p[i] != p[j + 1])
+            j = ne[j];
+        if (p[i] == p[j + 1])
+            j++;
+        ne[i] = j;
+    }
+    for (int i = 1, j = 0; i <= m; i++) {
+        while (j && s[i] != p[j + 1])
+            j = ne[j];
+        if (s[i] == p[j + 1])
+            j++;
+        if (j == n) {
+            // printf("%d ", i - n);
+            ans.push_back(i - n);
+            j = ne[j];
+        }
+    }
+    return ans;
+}
