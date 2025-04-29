347e7aba0f2c4fa3527ef9272da94d10a4b86ab8
Dezhonger <373440955@qq.com>
2025-04-30 01:20:54 +0800

    Update 阶乘和阶乘逆元.cpp

--- 数学/阶乘和阶乘逆元.cpp
+++ 数学/阶乘和阶乘逆元.cpp
@@ -4,6 +4,7 @@ int f[maxn], g[maxn];
 
 
 void factorial() {
+  // 多次调用，mod不同的话，需要     memset(f, 0, sizeof f); memset(g, 0, sizeof g);
   for (int i = 0; i < maxn; i += 1) f[i] = i ? 1LL * f[i - 1] * i % mod : 1;
   for (int i = 1; i < maxn; i += 1) g[i] = i == 1 ? 1 : 1LL * (mod - mod / i) * g[mod % i] % mod;
   for (int i = 0; i < maxn; i += 1) g[i] = g[i] ? 1LL * g[i - 1] * g[i] % mod : 1;
