1943379b9fed7346a463467a30910acee0ef0ec4
Dezhonger <373440955@qq.com>
2025-05-09 22:55:41 +0800

    Update 阶乘和阶乘逆元.cpp

--- 数学/阶乘和阶乘逆元.cpp
+++ 数学/阶乘和阶乘逆元.cpp
@@ -9,3 +9,9 @@ void factorial() {
   for (int i = 1; i < maxn; i += 1) g[i] = i == 1 ? 1 : 1LL * (mod - mod / i) * g[mod % i] % mod;
   for (int i = 0; i < maxn; i += 1) g[i] = g[i] ? 1LL * g[i - 1] * g[i] % mod : 1;
 }
+
+// 组合数
+LL C(int n, int m) {
+    if (n < m) return 0;
+    return 1LL * f[n] * g[m] % mod * g[n - m] % mod;
+}
