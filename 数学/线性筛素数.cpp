45b6ea47db2e58a0d8988522279f6360f1ee9e87
Dezhonger <373440955@qq.com>
2025-05-12 22:26:41 +0800

    Update 线性筛素数.cpp

--- 数学/线性筛素数.cpp
+++ 数学/线性筛素数.cpp
@@ -14,50 +14,47 @@ void get_primes(int n)
     }
 }
 
-
-
 // structure format
 struct Prime {
-	vector<int> primes;
-	vector<bool> st;
-	int n;
-	
-	Prime(int n) :n(n) {
-		st = vector<bool>(n + 1, false);
-	}
+    vector<int> primes;
+    vector<bool> st;
+    int n;
 
-	void get_primes()
-	{
-		for (int i = 2; i <= n; i ++ )
-		{
-			if (!st[i]) primes.push_back(i);
-			for (int j = 0; primes[j] <= n / i; j ++)
-			{
-				st[primes[j] * i] = true;
-				if (i % primes[j] == 0) break;
-			}
-		}
-	}
-	
-	int get_num() {
-		return primes.size();
-	}
-	
-	void print() {
-		cout << "num: " << get_num() << endl;
-		for (int i = 0; i < get_num(); i++) {
-			cout << primes[i] << " ";
-		}
-		cout << endl;
-	}
-	
-	void print(int m) {
-		cout << "num: " << get_num() << endl;
-		for (int i = 0; i < min(m, get_num()); i++) {
-			cout << primes[i] << " ";
-		}
-		cout << endl;
-	}
+    Prime(int n) : n(n) {
+        st = vector<bool>(n + 1, false);
+    }
+
+    void get_primes() {
+        for (int i = 2; i <= n; i++) {
+            if (!st[i])
+                primes.push_back(i);
+            for (int j = 0; primes[j] <= n / i; j++) {
+                st[primes[j] * i] = true;
+                if (i % primes[j] == 0)
+                    break;
+            }
+        }
+    }
+
+    int get_num() {
+        return primes.size();
+    }
+
+    void print() {
+        cout << "num: " << get_num() << endl;
+        for (int i = 0; i < get_num(); i++) {
+            cout << primes[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void print(int m) {
+        cout << "num: " << get_num() << endl;
+        for (int i = 0; i < min(m, get_num()); i++) {
+            cout << primes[i] << " ";
+        }
+        cout << endl;
+    }
 };
 
 int main() {
