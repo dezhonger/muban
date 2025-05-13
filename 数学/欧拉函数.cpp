278f2641edae14c4982e5851f62237d35e6e1b30
Dezhonger <373440955@qq.com>
2025-05-13 22:04:07 +0800

    Update 欧拉函数.cpp

--- 数学/欧拉函数.cpp
+++ 数学/欧拉函数.cpp
@@ -12,3 +12,29 @@ int phi(int x) {
         res = res / x * (x - 1);
     return res;
 }
+
+
+// 线性筛求欧拉函数
+// https://oi-wiki.org/math/number-theory/sieve/#%E7%AD%9B%E6%B3%95%E6%B1%82%E6%AC%A7%E6%8B%89%E5%87%BD%E6%95%B0
+vector<int> pri;
+bool not_prime[N];
+int phi[N];
+
+void pre(int n) {
+  phi[1] = 1;
+  for (int i = 2; i <= n; i++) {
+    if (!not_prime[i]) {
+      pri.push_back(i);
+      phi[i] = i - 1;
+    }
+    for (int pri_j : pri) {
+      if (i * pri_j > n) break;
+      not_prime[i * pri_j] = true;
+      if (i % pri_j == 0) {
+        phi[i * pri_j] = phi[i] * pri_j;
+        break;
+      }
+      phi[i * pri_j] = phi[i] * phi[pri_j];
+    }
+  }
+}
