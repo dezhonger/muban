7a67f99b0119bd73ebfa46a0e904861351ddd28b
Dezhonger <373440955@qq.com>
2025-05-14 17:08:43 +0800

    Update 三分.cpp

--- misc/三分.cpp
+++ misc/三分.cpp
@@ -1,6 +1,5 @@
 这个是一个单峰函数（是“先增后减”或“先减后增”），可以用三分法优化求最大值。
 
-
 // 三分法找最大值
 // [l, r]
 while (r - l > 5) {
@@ -8,6 +7,7 @@ while (r - l > 5) {
     ll m2 = r - (r - l) / 3;
     ll val1 = compute(m1);
     ll val2 = compute(m2);
+    // 求最小值需要把这里反向
     if (val1 < val2)
         l = m1;
     else
