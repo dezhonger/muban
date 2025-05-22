b39bdf1c5af7e534f4b623c1cab8f635ca66a63c
Dezhonger <373440955@qq.com>
2025-05-22 14:41:18 +0800

    Update 随机数.cpp

--- 数学/随机数.cpp
+++ 数学/随机数.cpp
@@ -24,3 +24,38 @@ int main() {
     }
     return 0;
 }
+
+// 整数均匀分布
+#include <iostream>
+#include <random>
+
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // [lower, upper]
+    int lower = 1, upper = 100;
+    std::uniform_int_distribution<> dis(lower, upper);
+
+    for (int i = 0; i < 5; ++i)
+        std::cout << dis(gen) << ' ';
+    std::cout << '\n';
+
+    return 0;
+}
+
+// 浮点数均匀分布
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    [lower, upper)
+    double lower = 0.0, upper = 1.0;
+    std::uniform_real_distribution<> dis(lower, upper);
+
+    for (int i = 0; i < 5; ++i)
+        std::cout << dis(gen) << ' ';
+    std::cout << '\n';
+
+    return 0;
+}
