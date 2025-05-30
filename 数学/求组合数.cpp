ae3c286d750ee3ded5f3262d6f29951454a9dd48
Dezhonger <373440955@qq.com>
2025-05-30 21:24:27 +0800

    Update 求组合数.cpp

--- 数学/求组合数.cpp
+++ 数学/求组合数.cpp
@@ -29,3 +29,11 @@ LL C(int n, int k) {
 	return res;
 }
 
+
+
+vector<vector<LL>> C(n + 1, vector<LL>(n + 1, 0));
+for (int i = 0; i <= n; i++)
+C[i][0] = 1;
+for (int i = 1; i <= n; i++)
+for (int j = 1; j <= i; j++)
+    (C[i][j] = C[i - 1][j] + C[i - 1][j - 1]) %= mod;
