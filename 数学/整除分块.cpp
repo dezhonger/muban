7d7eb70f4d36d94ce687a9333f58724735772c34
Dezhonger <373440955@qq.com>
2025-07-12 20:47:33 +0800

    Create 整除分块.cpp

---
+++ 数学/整除分块.cpp
@@ -0,0 +1,10 @@
+//  S = sum_{d=1..N} floor(N/d) ，O(sqrt(N))
+LL f(int N) {
+    LL S = 0;
+    for (LL i = 1, j; i <= N; i = j + 1) {
+        LL q = N / i;
+        j = N / q;
+        S = (S + ((__int128)q * (j - i + 1)) % MOD) % MOD;
+    }
+    return S;
+}
